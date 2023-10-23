4ff7958fb87a7eccd373e23e43bb7d13190ceb06
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-10-23 19:46:33 +0200

    feat: my age updates automatically

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
+const birthdate = "2002-05-25";
+
+function calculateAge(birthdate) {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function updateAge() {
+    document.getElementById("age").innerHTML = calculateAge(birthdate);
+}
+
+updateAge();
+
+setInterval(updateAge, 365*24*60*60*1000);
+
 const currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML =` ${currentYear} `;
+document.getElementById("copyright").innerHTML =`© ${currentYear}  Tjerk Symens`;
 
 document.querySelector('#div_homeease').addEventListener('click', () => {
     window.open('https://homeease.azurewebsites.net/index.php', '_blank');
