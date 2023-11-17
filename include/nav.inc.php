1f697e3d73db03d1d67f2a9f9f3df04d2555467f
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-11-17 16:37:42 +0100

    feat: add meta descriptio, keywords and author

--- include/nav.inc.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php" id="Home_btn">Tjerk Symens</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="portfolio.php">Portfolio</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-</nav>
